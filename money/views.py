ee29908c93921725710d2ea0bd02aefc4dbe37f4
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-02 17:17:37 +0530

    atomic removed

--- money/views.py
+++ money/views.py
@@ -20,33 +20,26 @@ class Moneydetail(LoginRequiredMixin, View):
         return render(request,'money/moneydetail.html',context)
 
     def post(self,request,*args,**kwargs):
-        with transaction.atomic():
-            try:
-                to = request.POST.get("to")
-                reason = request.POST.get("reason")
-                amt = request.POST.get("amount")
-                date = request.POST.get("date")
-                print(to,reason,amt,date)
+        to = request.POST.get("to")
+        reason = request.POST.get("reason")
+        amt = request.POST.get("amount")
+        date = request.POST.get("date")
+        print(to,reason,amt,date)
 
-                m = AddMoney(to=to,reason=reason,amount=float(amt),date=date,returned=False)
+        m = AddMoney(to=to,reason=reason,amount=float(amt),date=date,returned=False)
 
 
-                last = Totalmoney.objects.last()
-                print(last.total)
-                last.total = last.total + decimal.Decimal(float(amt))
-                last.updated_on = datetime.now()
+        last = Totalmoney.objects.last()
+        print(last.total)
+        last.total = last.total + decimal.Decimal(float(amt))
+        last.updated_on = datetime.now()
 
-                # last.save()
-                # m.save()
+        # last.save()
+        # m.save()
 
-                messages.success(request,'Balance Updated Successfully. Amount = '+str(amt))
+        messages.success(request,'Balance Updated Successfully. Amount = '+str(amt))
 
-                return redirect('addmoney')
-
-            except Exception as e:
-                print(str(e))
-                messages.warning(request, 'An error occured. Please Try Again!')
-                return redirect(reverse('addmoney'))
+        return redirect('addmoney')
 
 class Addmoney(LoginRequiredMixin, View):
     def get(self,request,*args,**kwargs):
@@ -56,32 +49,25 @@ class Addmoney(LoginRequiredMixin, View):
         return render(request,'money/addmoney.html',context)
 
     def post(self,request,*args,**kwargs):
-        with transaction.atomic():
-            try:
-                to = request.POST.get("to")
-                reason = request.POST.get("reason")
-                amt = decimal.Decimal(request.POST.get("amount"))
-                date = request.POST.get("date")
-                print(amt,reason,amt,date)
-                ret = False
-                if request.POST.get("checkme"):
-                    ret = True
+        to = request.POST.get("to")
+        reason = request.POST.get("reason")
+        amt = decimal.Decimal(request.POST.get("amount"))
+        date = request.POST.get("date")
+        print(amt,reason,amt,date)
+        ret = False
+        if request.POST.get("checkme"):
+            ret = True
 
-                m = AddMoney(to=to,reason=reason,amount=amt,date=date,returned=ret)
-                m.save()
-                last = Totalmoney.objects.last()
-                print(last.total,decimal.Decimal(float(amt)))
-                last.total = last.total + decimal.Decimal(float(amt))
-                last.updated_on = datetime.now()
+        m = AddMoney(to=to,reason=reason,amount=amt,date=date,returned=ret)
+        m.save()
+        last = Totalmoney.objects.last()
+        print(last.total,decimal.Decimal(float(amt)))
+        last.total = last.total + decimal.Decimal(float(amt))
+        last.updated_on = datetime.now()
 
-                last.save()
+        last.save()
 
 
-                messages.success(request,'Balance Updated Successfully. Amount = '+str(amt))
+        messages.success(request,'Balance Updated Successfully. Amount = '+str(amt))
 
-                return redirect('addmoney')
-
-            except Exception as e:
-                print(str(e))
-                messages.warning(request, 'An error occured. Please Try Again!')
-                return redirect(reverse('addmoney'))
+        return redirect('addmoney')
